cf336639b541c6ab0ef82ca317451f9fc05f0190
Swtg1337 <31216735+swtg1337@users.noreply.github.com>
2022-01-31 21:14:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 
         arrList[0] = {
             id: "1",
-            ready: false,
+            ready: true,
             text: "Задача 1",
         }
 
         arrList[1] = {
             id: "2",
-            ready: false,
+            ready: true,
             text: "Задача 2",
         }
 
@@ -20,24 +20,38 @@
 
 
 function filtration() {
+    document.getElementById('list').innerHTML = "";
     const fil_value = document.getElementById('filter').selectedIndex;
     
-    for (let i = 0; i < arrList.length; i++) {
-        if (arrList[i].text != "") {
-            
-            document.getElementById('list').innerHTML += '<p>' + arrList[i].text + '</p>' ;
-        } 
+    if (fil_value == 0) {           //вывод всего (условие если 0)
+        for (let i = 0; i < arrList.length; i++) {
+            if (arrList[i].text != "") {
+                 
+                document.getElementById('list').innerHTML += '<p>' + arrList[i].text + '</p>' ;
+                checkB(arrList[i].ready);
+            } 
+        }
     }
+
+    
 
 }    
 
+function checkB(stat){
+    const new1 = document.createElement('input');
+    new1.type = "checkbox";
+    if (stat == true){
+        new1.checked = true;
+    } else {
+        new1.checked = false;    
+    }
+    document.getElementById('list').appendChild(new1);
+}
 
-var new1 = document.createElement('input');
+filtration();
+
+/*var new1 = document.createElement('input');
 new1.type = "checkbox";
 new1.checked = true;
-new1.id = "new"+arrList[2].id;
-document.getElementById('list').appendChild(new1);
+document.getElementById('list').appendChild(new1);*/
 
-if (document.getElementsById('new3').checked == true) {
-    alert(new1.id);
-}
