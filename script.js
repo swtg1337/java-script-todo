5b3b093f95af33be474e8496e229943580bd7084
Swtg1337 <31216735+swtg1337@users.noreply.github.com>
2022-04-11 18:16:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,193 @@
-    const arrList = [];
+const checkboxData = [{
+    id: "1",
+    ready: true,
+    text: "Задача 1",
+  }, {
+    id: "2",
+    ready: false,
+    text: "Задача 2",
+  }, {
+    id: "3",
+    ready: false,
+    text: "Задача 3",
+  }, {
+    id: "4",
+    ready: true,
+    text: "Задача 4",
+  }];
 
-        arrList[0] = {
-            id: "1",
-            ready: true,
-            text: "Задача 1",
-        }
+const select = document.getElementById('filter');
+const list = document.getElementById('list');
 
-        arrList[1] = {
-            id: "2",
-            ready: true,
-            text: "Задача 2",
-        }
-
-        arrList[2] = {
-            id: "3",
-            ready: true,
-            text: "Задача 3",
-        }
-
-
-function filtration() {
-    document.getElementById('list').innerHTML = "";
-    const fil_value = document.getElementById('filter').selectedIndex;
-    
-    if (fil_value == 0) {           //вывод всего (условие если 0)
-        for (let i = 0; i < arrList.length; i++) {
-            if (arrList[i].text != "") {
-                 
-                document.getElementById('list').innerHTML += '<p>' + arrList[i].text + '</p>' ;
-                checkB(arrList[i].ready);
-            } 
-        }
-    }
-
-    
-
-}    
-
-function checkB(stat){
-    const new1 = document.createElement('input');
-    new1.type = "checkbox";
-    if (stat == true){
-        new1.checked = true;
-    } else {
-        new1.checked = false;    
-    }
-    document.getElementById('list').appendChild(new1);
+function ClearAll() {
+  list.innerHTML = " ";
 }
 
-filtration();
+select.addEventListener('change', function(){
+  const filter = select.value;
+  const filtered = filtration(checkboxData, filter);
+  //функция вывода массива после фильтрации. фильтрация должна вернуть массив для вывода
+  ClearAll();
+  //выовод массива с созданием инпутов
+  list.append(createInputsList(filtered))
+});
 
-/*var new1 = document.createElement('input');
+//вернуться и проверить ретёрн
+function filtration(arr, filter) {
+  switch (filter) {
+    case "all_list": 
+      return arr;
+
+    case "ready_list":
+      return arr.filter(function(ready) {
+        return ready.ready === true;
+      });
+      
+
+    case "not_ready_list":
+      return arr.filter(function(ready) {
+        return ready.ready === false;
+      });
+      
+
+    default: 
+      return arr;
+  }
+  
+};
+
+
+
+
+const createInputRow = ({
+  id,
+  text,
+  ready
+}) => {
+  const wrapper = document.createElement('div');
+
+  wrapper.append(createCheckboxEl(id, ready), createLabelEl(id, text))
+
+  return wrapper;
+}
+
+const createLabelEl = (id, text) => {
+  const label = document.createElement('label');
+  label.htmlFor = id;
+  label.innerText = text;
+
+  return label
+}
+
+const createCheckboxEl = (id, ready) => {
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.id = id;
+  checkbox.checked = ready;
+
+  return checkbox
+}
+
+const createInputsList = (inputsData) => {
+  const wrapper = document.createElement('div');
+  for (const item of inputsData) {
+    wrapper.append(createInputRow(item))
+  }
+  return wrapper
+}
+
+list.append(createInputsList(checkboxData));
+
+/*
+var new1 = document.createElement('input');
 new1.type = "checkbox";
 new1.checked = true;
-document.getElementById('list').appendChild(new1);*/
+document.getElementById('list').appendChild(new1);
 
+// очишаем элемент, в который будем рендерить контейнер с инпутами.
+const clearElement = (elem) => {
+  let child = elem.lastElementChild;
+  while (child) {
+    elem.removeChild(child);
+    child = elem.lastElementChild;
+  }
+}
+
+
+// обработчик событий на селект. фильтруем массив с данными инпутов и рендерим нужные.
+// по хорошему, это тоже отдельная функция, которая принимает элемент, в который нужно рендерить + массив и фильтр к нему тоже
+select.addEventListener('change', () => {
+  const filter = select.value;
+  const filtered = filterArray(checkboxData, filter)
+  clearElement(list);
+  list.append(createInputsList(filtered))
+
+})
+
+
+// получаем нужные данные из исходного массива
+function filterArray(arr, filter) {
+  switch (filter) {
+    case "all_list": {
+      return arr;
+    }
+    case "ready_list": {
+      return arr.filter((el) => el.ready === true);
+    }
+
+    case "not_ready_list": {
+      return arr.filter((el) => el.ready === false);
+    }
+
+    default: {
+      return arr;
+    }
+  }
+
+}
+
+
+const createLabelEl = (id, text) => {
+  const label = document.createElement('label');
+  label.htmlFor = id;
+  label.innerText = text;
+
+  return label
+}
+
+const createCheckboxEl = (id, ready) => {
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.id = id;
+  checkbox.checked = ready;
+
+  return checkbox
+}
+
+//создаем строчку инпут + текст. чтобы можно было кликать по тексту и менять состояние инпута, передаем одинаковые айди для обоих функций внутренних(лейбл ссылается на инпут по айди)
+const createInputRow = ({
+  id,
+  text,
+  ready
+}) => {
+  const wrapper = document.createElement('div');
+
+  wrapper.append(createCheckboxEl(id, ready), createLabelEl(id, text))
+
+  return wrapper;
+}
+
+
+// собираем все инпуты в один див, чтобы меньше обращений было к DOM дереву в браузере.
+const createInputsList = (inputsData) => {
+  const wrapper = document.createElement('div');
+
+  for (const item of inputsData) {
+    wrapper.append(createInputRow(item))
+  }
+
+  return wrapper
+}
+// первоначальный рендер
+list.append(createInputsList(checkboxData))
+*/
